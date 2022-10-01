15700008ce8bb69c2e6e77bdc329836717b3ef60
Reuben Atendido <K21048267@kcl.ac.uk>
2022-10-01 21:23:18 +0100

    attempt to extend prime v1 code to raise valueError when an input < 0 is recieved

--- primes.py
+++ primes.py
@@ -3,4 +3,22 @@
 
 def primes(number_of_primes):
     list = []
+    count = 0
+    if (number_of_primes <= 0):
+        raise ValueError("Value entered should be greater than 0")
+
+    for x in range(2,151):
+        prime = True
+        for i in range(2,x):
+            if x % i == 0:
+                prime = False 
+        if prime:
+            if count != number_of_primes:
+                list.append(x)
+                count+=1
+            else:
+                break
     return list
+
+# x=input("enter: ")
+# print(primes(int(x)))
